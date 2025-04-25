fdfb387ffc90d3f354233fb6d2c9e07f72f58a44
S_H\shimo <shimobou8810@icloud.com>
2025-04-25 19:36:03 +0900

    04/25 shimo publicをprivateの変更

--- Assets/Develop/shimo/PlayerJoinLog.cs
+++ Assets/Develop/shimo/PlayerJoinLog.cs
@@ -4,13 +4,13 @@ using UnityEngine.InputSystem;
 public class PlayerJoinLog : MonoBehaviour
 {
     // プレイヤー入室時に受け取る通知
-    public void OnPlayerJoined(PlayerInput playerInput)
+    private void OnPlayerJoined(PlayerInput playerInput)
     {
         Debug.Log($"プレイヤー#{playerInput.user.index}が入室！");
     }
 
     // プレイヤー退室時に受け取る通知
-    public void OnPlayerLeft(PlayerInput playerInput)
+    private void OnPlayerLeft(PlayerInput playerInput)
     {
         Debug.Log($"プレイヤー#{playerInput.user.index}が退室！");
     }
