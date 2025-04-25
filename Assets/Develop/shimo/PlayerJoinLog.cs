f685598341d1cf8ff9139f7402d5ecfd2008b319
S_H\shimo <shimobou8810@icloud.com>
2025-04-25 21:23:08 +0900

    04/25 shimo 画面の分割、プレイヤーの制御

--- Assets/Develop/shimo/PlayerJoinLog.cs
+++ Assets/Develop/shimo/PlayerJoinLog.cs
@@ -4,13 +4,13 @@ using UnityEngine.InputSystem;
 public class PlayerJoinLog : MonoBehaviour
 {
     // プレイヤー入室時に受け取る通知
-    private void OnPlayerJoined(PlayerInput playerInput)
+    public void OnPlayerJoined(PlayerInput playerInput)
     {
         Debug.Log($"プレイヤー#{playerInput.user.index}が入室！");
     }
 
     // プレイヤー退室時に受け取る通知
-    private void OnPlayerLeft(PlayerInput playerInput)
+    public void OnPlayerLeft(PlayerInput playerInput)
     {
         Debug.Log($"プレイヤー#{playerInput.user.index}が退室！");
     }
