281b90e429e72c2615cf8a0b9d56b3f220b9029a
e1c23007 <e1c23007@oit.ac.jp>
2025-04-28 08:31:57 +0900

    2025/4/28 梅園　自分のシーン追加

---
+++ Assets/_Main/Script/UI/Timer.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
