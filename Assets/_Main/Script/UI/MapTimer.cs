58c3eeb47ac08a4d77d6fb4907063d04110d0b7d
e1c23007 <e1c23007@oit.ac.jp>
2025-05-13 21:01:43 +0900

    2025/13 梅園　タグ追加

--- Assets/_Main/Script/UI/MapTimer.cs
+++ Assets/_Main/Script/UI/MapTimer.cs
@@ -1,8 +1,16 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MapTimer : MonoBehaviour
 {
+    [SerializeField] private float _startTime = 300;
+
     private GameTimer _gameTimer;
+    private Image _mapTimerImage;
+    private float timeLeft; 
+
+
+
     void Start()
     {
         if (GameObject.FindWithTag("GameTimer"))
@@ -14,6 +22,18 @@ public class MapTimer : MonoBehaviour
             Debug.LogError("GameTimer オブジェクトが見つかりません。タグを確認してください。");
         }
 
+        if (GameObject.FindWithTag("MapTimer"))
+        {
+            _mapTimerImage = GameObject.FindWithTag("MapTimer").GetComponent<Image>();
+        }
+        else
+        {
+            Debug.LogError("MapTimer オブジェクトが見つかりません。タグを確認してください。");
+        }
+
+        timeLeft = _startTime;
+        _mapTimerImage.fillAmount = 1f;
+
         _gameTimer.StartTimer(); //デバック用！！！！！！！！！！！！！！！！！！！！！
 
     }
@@ -21,6 +41,22 @@ public class MapTimer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (_gameTimer == null || _mapTimerImage == null) return;
 
+        if (timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+            float fillAmount = timeLeft / _startTime;
+            _mapTimerImage.fillAmount = fillAmount;
+
+            //_mapTimerImage.color = Color.Lerp(Color.red, Color.green, fillAmount);
+        }
+        else
+        {
+            _mapTimerImage.fillAmount = 0f;
+
+            //タイマーが0になったらここに処理を書く!!!!!!!!!!!!!!!!!!!!
+        }
     }
+        
 }
